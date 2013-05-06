5ada32dc332a6cc2aa13ca75f50a48112da4e5f9
sudolee <desoxydate@gmail.com>
2013-05-06 23:05:18 +0800

    1, add debug interface for nand. 2, all fork from uart 3, readly go.... nand coding....

--- nand/nand.c
+++ nand/nand.c
@@ -2,11 +2,18 @@
 #include "io.h"
 #include "uart.h"
 
+#ifdef DEBUG_LEDS
+#include "debug.h"
+#endif /* DEBUG_LEDS */
+
 struct nand_res {
 };
 
 void nand_main(void)
 {
+#ifdef DEBUG_LEDS
+	lights(5);
+#endif
 	while(1)
-		puts(get_port_entry(0), "hello world....\n");
+		puts(get_port_entry(0), "Hello, the curel world...\n");
 }
